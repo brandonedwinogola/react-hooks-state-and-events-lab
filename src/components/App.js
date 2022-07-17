5270c4429a7f8ddab4c423c6b1c6d5c1343703d5
brandonedwinogola <brandon14ogola@gmail.com>
2022-07-17 11:57:30 +0300

    completed assignment on state and events

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,31 @@ import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
+
+
+
+
+
+  const darkMode = useDarkMode();
+  const [theme, setTheme] = React.useState(darkTheme);
+  React.useEffect(() => {
+    setTheme(darkMode?.value ? darkTheme : lightTheme);
+  }, [darkMode.value]);
+
+
+
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
   const appClass = false ? "App dark" : "App light"
 
+  
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button className="btn-theme" type="button" onClick={darkMode.toggle}> {theme === "dark-mode" ? "Light-mode" : "Dark-mode" }Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -6,6 +6,7 @@ function Item({ name, category }) {
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
+      <button className="remove">Remove From Cart</button>
     </li>
   );
 }
