ce7cbaba62af7ae33cc9b59ea076543c66187763
brandonedwinogola <brandon14ogola@gmail.com>
2022-07-17 15:07:05 +0300

    completed assignment on state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 
-
-
-
-
-  const darkMode = useDarkMode();
-  const [theme, setTheme] = React.useState(darkTheme);
-  React.useEffect(() => {
-    setTheme(darkMode?.value ? darkTheme : lightTheme);
-  }, [darkMode.value]);
-
-
-
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  
 
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
 
   
   return (
-    <div className={appClass}>
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button className="btn-theme" type="button" onClick={darkMode.toggle}> {theme === "dark-mode" ? "Light-mode" : "Dark-mode" }Dark Mode</button>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
